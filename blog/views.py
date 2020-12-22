eb3d42c4253477a03261a508727a817cd9e167f0
mrahman <mrahmandusp@gmail.com>
2020-12-22 09:47:17 +0600

    second commit

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,53 @@ from .models import Post, BlogComment
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+import math
 from django.core.paginator import Paginator    # this for pagination
 
 
 # Create your views here.
-def bloghome(request):
-    allposts = Post.objects.all()
-    context = {'allPosts': allposts}
-    return render(request, "blog/blogHome.html", context)
+#def bloghome(request):
+#    allposts = Post.objects.all()
+#    context = {'allPosts': allposts}
+#    return render(request, "blog/blogHome.html", context)
 
+
+def bloghome(request):
+    no_of_posts = 2
+    #if request.GET("pageno")
+    page = request.GET.get('page')
+    if page is None:
+        page = 1
+    else:
+        page = int(page)
+
+
+    """
+    1: 0-3
+    2: 3-6
+    3: 6-9
+    
+    1: 0 to  0 + no_of_posts
+    2: no_of_posts to no_of_posts + no_of_posts
+    3: no_of_posts + no_of_posts to no_of_posts+ no_of_posts +no_of_posts
+
+    (page -1)* no_of_posts to page* no_of_posts
+    """
+
+    allposts = Post.objects.all()
+    length = len(allposts)
+    allposts = allposts[(page-1)*no_of_posts: page* no_of_posts]
+    if page>1:
+        prev = page - 1
+    else:
+        prev = None
+
+    if page<math.ceil(length / no_of_posts):
+        nxt = page + 1
+    else: nxt = None
+    
+    context = {'allPosts': allposts, 'prev': prev, 'nxt': nxt}
+    return render(request, "blog/blogHome.html", context)
 
 def search(request):
     query = request.GET['query']
